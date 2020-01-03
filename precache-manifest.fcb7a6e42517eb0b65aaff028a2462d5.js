fc93c9c5289739c1058f81019213e7eb0774508f
offminded <offminded@gmail.com>
2020-01-04 00:13:08 +0300

    Updates

---
+++ precache-manifest.fcb7a6e42517eb0b65aaff028a2462d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea5884dfd4acf0d48bc5d1382ccc984",
+    "url": "/rct1/index.html"
+  },
+  {
+    "revision": "9ee7c1228cba033ee6ce",
+    "url": "/rct1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "11a375d05ddc3aeac8df",
+    "url": "/rct1/static/js/2.8a6145b6.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/rct1/static/js/2.8a6145b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ee7c1228cba033ee6ce",
+    "url": "/rct1/static/js/main.5f955066.chunk.js"
+  },
+  {
+    "revision": "324e42bb57bc853841f5",
+    "url": "/rct1/static/js/runtime-main.bd9ffcff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rct1/static/media/logo.5d5d9eef.svg"
+  }
+]);
